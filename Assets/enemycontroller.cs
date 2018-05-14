eaa43e26244380e60f614e9c581e3b071b4a1e49
Yuuki Ooguro <kitkat.jaja@gmail.com>
2018-05-14 21:02:28 +0900

    shootingGame

---
+++ Assets/enemycontroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class enemycontroller : MonoBehaviour {
+	public GameObject target;
+	public GameObject stage;
+	float stageX;
+	float stageY;
+	float stageZ;
+	float time;
+
+	// Use this for initialization
+	void Start () {
+		
+		// オブジェクトの大きさを取得している
+		stageX = stage.GetComponent<Renderer> ().bounds.size.x;
+		stageY = stage.GetComponent<Renderer> ().bounds.size.y;
+		stageZ = stage.GetComponent<Renderer> ().bounds.size.z;
+
+		generateEnemy ();
+		time = 0;
+	}
+	// Update is called once per frame
+	void Update () {
+		//GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		time += Time.deltaTime;
+		if (time >= 5) {
+			time = 0;
+			generateEnemy ();
+		}
+	}
+	void generateEnemy(){
+		for (int i = 0; i < 10; i++) {
+			Instantiate (target, stage.transform.position + new Vector3 (Random.Range (-stageX / 2, stageX / 2),
+				Random.Range (-stageY / 2, stageY / 2),
+				Random.Range (-stageZ / 2, stageZ / 2)), Quaternion.identity);
+		}
+	}
+}
---
+++ Assets/raycontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class raycontroller : MonoBehaviour {
+	public static int score = 0;
+	public GameObject bullet;
+	public float speed = 60f;
+	GameObject bullet1 ;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			bullet1 = (GameObject)Instantiate (bullet, gameObject.transform.position, Quaternion.identity);
+			Vector3 direction = ray.direction;
+			bullet1.gameObject.GetComponent<Rigidbody> ().velocity = direction * speed;
+		}
+		if (bullet1.transform.position.z > 500) {
+			Destroy (bullet1.gameObject);
+		}
+		if (score >= 40) {
+			SceneManager.LoadScene ("gameclear");
+		}
+	}
+}
